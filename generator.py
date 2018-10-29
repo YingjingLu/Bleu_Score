078eb1df687b92240e78d9cc4152cccfdda77f65
YingjingLu <yingjinglu2019us@gmail.com>
2018-10-28 20:03:00 -0400

    generator added

--- generator.py
+++ generator.py
@@ -1,14 +1,68 @@
 import numpy as np 
 from util import * 
 import pickle
-from vocab import VocabEntry 
+from vocab import VocabEntry, tokenize
+import random 
 
-def greedy_generator_single_sentence( sentence, vocab, secure_n_gram, replace_prob, replace_with_top, displacement_range, displacement_prob ):
+def greedy_generator_single_sentence( sentence, vocab, secure_n_gram, replace_prob, replace_with_top, insert_prob, insert_with_top, displacement_range, displacement_prob ):
     """
     Take in a single sentence and output the 
     """
+    # convet to number array
+    word_list = tokenize( sentence )
+    idx_list = vocab.single_sentence2ids( word_list )
+    length = len( idx_list )
+    cur_length = 0
+    idx_array = []
 
-def greedy_generator( in_file, out_file, vocab, secure_n_gram, replace_prob, replace_with_top, displacement_range, displacement_prob ):
+    replace_lst = np.arange( ( 1, replace_with_top ) )
+    replace_p = np.arange( ( replace_with_top, 1 ) )
+    replace_p = replace_p / np.sum( replace_p )
+    
+    insert_lst = np.arange( ( 1, insert_with_top ) )
+    insert_p = np.arange( ( insert_with_top, 1 ) )
+    insert_p = insert_p / np.sum( insert_p )
+    # break sentences into n_grams with top n gram being secure_n_gram
+    while cur_length < length:
+        slice_length = random.randint( 1, secure_n_gram )
+        idx_array.append( idx_list[ cur_length: cur_length + slice_length ] )
+        cur_length += slice_length
+
+    # randomly replace words in n_gram
+    for l in range( len( idx_array ) ):
+        for i in range( len( idx_array[ l ] ) ):
+            if random.random() < replace_prob:
+                idx_array[ l ][ i ] = np.random.choice( replace_lst, p = replace_p )
+    
+    # randomly insert n_gram in the sentence
+    for l in range( len( idx_array ) ):
+        if random.random() < insert_prob:
+            insert_len = random.randint( 1, secure_n_gram )
+            ins = []
+            for _ in range( insert_len ):
+                ins.append( np.random.choice( insert_lst, p = insert_p ) )
+            id_array[ l ] += ins 
+    # randomly displace
+    cur_len = 0
+    ret = []
+    while cur_len < length:
+        displace_len = random.randint( 1, displacement_range )
+        add = idx_array[ cur_len: cur_len + displace_len ]
+        if random.random() < displacement_prob:
+            shuffle_idx = np.arange( displace_len, dtype = np.int )
+            np.random.shuffle( shuffle_idx )
+            add_new = []
+            for i in shuffle_idx:
+                add_new.append( add[ i ] )
+            add = add_new
+        cur_len += displace_len
+        ret += add
+    res = []
+    for item in ret:
+        res += item 
+    return vocab.id2single_sentence( res )
+
+def greedy_generator( in_file, out_file, vocab, secure_n_gram, replace_prob, replace_with_top,insert_prob, insert_with_top, displacement_range, displacement_prob ):
     """
     Replace a src sentence with random vocab, and displacement in certain probability
     Args:
@@ -23,5 +77,17 @@ def greedy_generator( in_file, out_file, vocab, secure_n_gram, replace_prob, rep
     Returns:
         list of sentences 
     """
-    pass
-    
+    in_f = open( in_file, "r", encoding = "utf-8" )
+    out_f = open( out_file, "w" )
+
+    line = in_f.readline()
+    while line != "":
+        res = greedy_generator_single_sentence( line, vocab, secure_n_gram, replace_prob,
+                                                replace_with_top, insert_prob, insert_with_top,
+                                                displacement_range, displacement_prob )
+        out_f.write( res + "\n" )
+
+        line = in_f.readline()
+    in_f.close()
+    out_f.close()
+    return 0
